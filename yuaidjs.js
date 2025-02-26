1b889ca73239b30bcfa6a2380bdcc8989a4de04c
cthvlab <60858283+cthvlab@users.noreply.github.com>
2025-02-26 19:01:38 +0300

    Update yuaidjs.js

--- yuaidjs.js
+++ yuaidjs.js
@@ -1,135 +1,131 @@
+// Глобальный объект для фреймворка YuaidJS
+window.YuaidJS = window.YuaidJS || {};
+
 // Класс для управления реактивными данными
-class Reactive {
-  constructor(data) {
-    this.data = data;
-    this.listeners = [];
-  }
+YuaidJS.Reactive = function(data) {
+  this.data = data;
+  this.listeners = [];
+};
 
-  set(key, value) {
-    this.data[key] = value;
-    this.notify();
-  }
+YuaidJS.Reactive.prototype.set = function(key, value) {
+  this.data[key] = value;
+  this.notify();
+};
 
-  get(key) {
-    return this.data[key];
-  }
+YuaidJS.Reactive.prototype.get = function(key) {
+  return this.data[key];
+};
 
-  notify() {
-    this.listeners.forEach(listener => listener());
-  }
+YuaidJS.Reactive.prototype.notify = function() {
+  this.listeners.forEach(function(listener) { listener(); });
+};
 
-  subscribe(listener) {
-    this.listeners.push(listener);
-  }
+YuaidJS.Reactive.prototype.subscribe = function(listener) {
+  this.listeners.push(listener);
+};
 
-  async fetchData(url) {
-    const response = await fetch(url);
-    this.set('data', await response.json());
-  }
+YuaidJS.Reactive.prototype.fetchData = async function(url) {
+  const response = await fetch(url);
+  this.set('data', await response.json());
+};
 
-  computed(key, computeFn) {
-    Object.defineProperty(this.data, key, {
-      get: computeFn,
-      enumerable: true,
-    });
-  }
-}
+YuaidJS.Reactive.prototype.computed = function(key, computeFn) {
+  Object.defineProperty(this.data, key, {
+    get: computeFn,
+    enumerable: true
+  });
+};
 
 // Класс для создания компонентов
-class Component {
-  constructor(template, data) {
-    this.template = template;
-    this.reactiveData = new Reactive(data);
-    this.reactiveData.subscribe(() => this.update());
-    this.el = this.render();
-  }
+YuaidJS.Component = function(template, data) {
+  this.template = template;
+  this.reactiveData = new YuaidJS.Reactive(data);
+  this.reactiveData.subscribe(this.update.bind(this));
+  this.el = this.render();
+};
 
-  render() {
-    const html = this.template(this.reactiveData.data);
-    const div = document.createElement('div');
-    div.innerHTML = html;
-    return div;
-  }
+YuaidJS.Component.prototype.render = function() {
+  const html = this.template(this.reactiveData.data);
+  const div = document.createElement('div');
+  div.className = 'yuaidjs-component'; // Добавляем класс с названием фреймворка
+  div.innerHTML = html;
+  return div;
+};
 
-  update() {
-    const newHtml = this.template(this.reactiveData.data);
-    const tempDiv = document.createElement('div');
-    tempDiv.innerHTML = newHtml;
-    this.el.innerHTML = tempDiv.innerHTML;
-  }
+YuaidJS.Component.prototype.update = function() {
+  const newHtml = this.template(this.reactiveData.data);
+  const tempDiv = document.createElement('div');
+  tempDiv.innerHTML = newHtml;
+  this.el.innerHTML = tempDiv.innerHTML;
+};
 
-  getElement() {
-    return this.el;
-  }
-}
+YuaidJS.Component.prototype.getElement = function() {
+  return this.el;
+};
 
 // Компонент для отображения контента
-class ContentContainer extends Component {
-  constructor(data, router) {
-    super(ContentContainer.template, data);
-    this.router = router;
-    if (data.data && data.data.hits && data.data.hits.length > 0) {
-      this.loadContent();
-    }
-    this.reactiveData.subscribe(() => this.loadContent());
+YuaidJS.ContentContainer = function(data, router) {
+  YuaidJS.Component.call(this, YuaidJS.ContentContainer.template, data);
+  this.router = router;
+  if (data.data && data.data.hits && data.data.hits.length > 0) {
+    this.loadContent();
   }
+  this.reactiveData.subscribe(this.loadContent.bind(this));
+};
 
-  static template(data) {
-    return `
-      <div class="content-container">
-        ${data.content || 'Загрузка контента...'}
-      </div>
-    `;
-  }
+YuaidJS.ContentContainer.prototype = Object.create(YuaidJS.Component.prototype);
+YuaidJS.ContentContainer.prototype.constructor = YuaidJS.ContentContainer;
 
-  async loadContent() {
-    const hits = this.reactiveData.get('data')?.hits;
-    if (hits && hits.length > 0) {
-      const contentUrl = `${hits[0].alias}_${hits[0].id}`;
-      try {
-        this.router.navigate(`/${contentUrl}`);
-        const response = await fetch(contentUrl);
-        const content = await response.text();
-        this.reactiveData.set('content', content);
-      } catch (error) {
-        this.reactiveData.set('content', 'Ошибка загрузки контента');
-      }
+YuaidJS.ContentContainer.template = function(data) {
+  return `
+    <div class="yuaidjs-content-container">
+      ${data.content || 'Загрузка контента...'}
+    </div>
+  `;
+};
+
+YuaidJS.ContentContainer.prototype.loadContent = async function() {
+  const hits = this.reactiveData.get('data')?.hits;
+  if (hits && hits.length > 0) {
+    const contentUrl = `${hits[0].alias}_${hits[0].id}`;
+    try {
+      this.router.navigate(`/${contentUrl}`);
+      const response = await fetch(contentUrl);
+      const content = await response.text();
+      this.reactiveData.set('content', content);
+    } catch (error) {
+      this.reactiveData.set('content', 'Ошибка загрузки контента (YuaidJS)');
     }
   }
-}
+};
 
 // Класс для управления роутингом
-class Router {
-  constructor(routes) {
-    this.routes = routes;
-    this.currentRoute = null;
-    window.addEventListener('popstate', this.handleRouteChange.bind(this));
-  }
+YuaidJS.Router = function(routes) {
+  this.routes = routes;
+  this.currentRoute = null;
+  window.addEventListener('popstate', this.handleRouteChange.bind(this));
+};
 
-  handleRouteChange() {
-    const path = window.location.pathname;
-    const route = this.routes[path] || this.routes['/'];
-    if (route) {
-      this.render(route);
-    }
+YuaidJS.Router.prototype.handleRouteChange = function() {
+  const path = window.location.pathname;
+  const route = this.routes[path] || this.routes['/'];
+  if (route) {
+    this.render(route);
   }
+};
 
-  navigate(path) {
-    window.history.pushState({}, '', path);
-    const route = this.routes[path] || this.routes['/'];
-    if (route) {
-      this.render(route);
-    }
+YuaidJS.Router.prototype.navigate = function(path) {
+  window.history.pushState({}, 'YuaidJS Route', path); // Добавляем название в title истории
+  const route = this.routes[path] || this.routes['/'];
+  if (route) {
+    this.render(route);
   }
+};
 
-  render(route) {
-    if (this.currentRoute) {
-      this.currentRoute.getElement().remove();
-    }
-    this.currentRoute = route;
-    document.body.appendChild(route.getElement());
+YuaidJS.Router.prototype.render = function(route) {
+  if (this.currentRoute) {
+    this.currentRoute.getElement().remove();
   }
-}
-
-// Экспортируем классы для использования в других файлах
-export { Reactive, Component, ContentContainer, Router };
+  this.currentRoute = route;
+  document.body.appendChild(route.getElement());
+};
